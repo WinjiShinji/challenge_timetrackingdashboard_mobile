ef93bab0788ffca6e07e74b12fcc2396f9793eee
Dominic Pullinger <118799360+WinjiShinji@users.noreply.github.com>
2024-06-05 09:26:49 +0100

    Card component

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,50 @@
-import { StyleSheet, View } from "react-native"
+import { ScrollView, StyleSheet, View } from "react-native"
 import CardProfile from "../components/CardProfile"
 import { AppProvider } from "@/context/AppContext"
 import { SafeAreaView } from "react-native-safe-area-context"
 import data from "../data/data.json"
-import CardTemplate from "@/components/CardTemplate"
+import CardComponent from "@/components/CardComponent"
 
 export default function Index() {
   const activityData = data[0]
 
   return (
     <SafeAreaView>
-      <View style={styles.main}>
+      <ScrollView style={styles.main}>
         <AppProvider>
           <CardProfile />
-          <CardTemplate
+          <CardComponent
             data={activityData.work}
-            image={require("../assets/images/icon-work.svg")}
+            image={require("../assets/images/icon-work.png")}
+            color={"hsl(15, 100%, 70%)"}
+          />
+          <CardComponent
+            data={activityData.play}
+            image={require("../assets/images/icon-play.png")}
+            color={"hsl(195, 74%, 62%)"}
+          />
+          <CardComponent
+            data={activityData.study}
+            image={require("../assets/images/icon-study.png")}
+            color={"hsl(348, 100%, 68%)"}
+          />
+          <CardComponent
+            data={activityData.exercise}
+            image={require("../assets/images/icon-exercise.png")}
+            color={"hsl(145, 58%, 55%)"}
+          />
+          <CardComponent
+            data={activityData.social}
+            image={require("../assets/images/icon-social.png")}
+            color={"hsl(264, 64%, 52%)"}
+          />
+          <CardComponent
+            data={activityData.selfcare}
+            image={require("../assets/images/icon-self-care.png")}
+            color={"hsl(43, 84%, 65%)"}
           />
         </AppProvider>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
---
+++ components/CardComponent.tsx
@@ -0,0 +1,151 @@
+import AppContext from "@/context/AppContext"
+import { useContext } from "react"
+import {
+  Button,
+  Image,
+  ImageURISource,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native"
+
+type Props = {
+  data: {
+    title: string
+    timeframes: {
+      daily: {
+        current: number
+        previous: number
+      }
+      weekly: {
+        current: number
+        previous: number
+      }
+      monthly: {
+        current: number
+        previous: number
+      }
+    }
+  }
+  image: ImageURISource
+  color: string
+}
+
+export default function CardComponent({ data, image, color }: Props) {
+  const { state } = useContext(AppContext)
+
+  return (
+    <View style={styles.container}>
+      <View style={[styles.header, { backgroundColor: color }]}>
+        <Image
+          style={styles.img}
+          source={image}
+          height={50}
+          width={50}
+          alt=""
+        />
+      </View>
+
+      <View style={styles.body}>
+        <View style={styles.columnA}>
+          <Text style={styles.title}>{data.title}</Text>
+          <View>
+            <Text style={styles.hours}>{`${
+              state.daily === true
+                ? data.timeframes.daily.current
+                : state.weekly === true
+                ? data.timeframes.weekly.current
+                : state.monthly === true
+                ? data.timeframes.monthly.current
+                : 0
+            }hrs`}</Text>
+          </View>
+        </View>
+
+        <View style={styles.columnB}>
+          <View>
+            <View style={styles.button}>
+              <Button title=". . ." />
+            </View>
+            <Text style={styles.state}>
+              {`${
+                state.daily === true
+                  ? "yesterday -"
+                  : state.weekly === true
+                  ? "last week -"
+                  : state.monthly === true
+                  ? "last month -"
+                  : "no state found!"
+              } ${
+                state.daily === true
+                  ? data.timeframes.daily.previous
+                  : state.weekly === true
+                  ? data.timeframes.weekly.previous
+                  : state.monthly === true
+                  ? data.timeframes.monthly.previous
+                  : "0"
+              }hrs`}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  body: {
+    width: "100%",
+    flexDirection: "row",
+    borderRadius: 8,
+    backgroundColor: "hsl(235, 46%, 20%)",
+    transform: [{ translateY: -16 }],
+  },
+  button: {
+    width: 48,
+    justifyContent: "flex-start",
+    alignSelf: "flex-end",
+    marginBottom: 8,
+  },
+  columnA: {
+    justifyContent: "space-evenly",
+    width: "50%",
+    padding: 16,
+  },
+  columnB: {
+    justifyContent: "space-evenly",
+    width: "50%",
+    padding: 16,
+  },
+  container: {},
+  header: {
+    height: 72,
+    overflow: "hidden",
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  hours: {
+    color: "rgba(255, 255, 255, 0.87)",
+    fontSize: 32,
+    fontWeight: "400",
+  },
+  img: {
+    alignSelf: "flex-end",
+    transform: [{ scale: 1.4 }, { translateX: -16 }],
+  },
+  state: {
+    color: "hsl(236, 100%, 87%)",
+    textTransform: "capitalize",
+    fontSize: 16,
+    textAlign: "right",
+    marginTop: 8,
+  },
+  title: {
+    color: "rgba(255, 255, 255, 0.87)",
+    textTransform: "capitalize",
+    fontSize: 24,
+    fontWeight: "700",
+    marginTop: 8,
+    marginBottom: 8,
+  },
+})
